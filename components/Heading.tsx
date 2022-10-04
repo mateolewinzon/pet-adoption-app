2f4956f793ba6a367048f6d3fb7575d81e046e3d
unknown <mateolewinzon@gmail.com>
2022-10-04 12:58:03 -0300

    fix: Heading.tsx error

---
+++ components/Heading.tsx
@@ -0,0 +1,20 @@
+import { TextComponent } from "components";
+
+type Props = {
+  children: React.ReactNode,
+  className?: string,
+  as?: React.ElementType,
+}
+
+export const Heading = ({ children, className, as = "h1", ...props }: Props) => {
+  return (
+    <TextComponent
+      {...props}
+      defaultStyles={"text-5xl font-bold text-black dark:text-white inline"}
+      overrideStyles={className}
+      as={as}
+    >
+      {children}
+    </TextComponent>
+  );
+}
