3b355b2ca509f3682ecd09430ce0c8a4a27a7fb9
unknown <mateolewinzon@gmail.com>
2022-11-18 12:30:03 -0300

    fix: removed api call on getstaticprops

--- service/privateApi.ts
+++ service/privateApi.ts
@@ -1,17 +1,13 @@
 import fetcher from "utils/fetch";
 
-const baseURL = "api/";
+const baseURL = process.env.NEXT_PUBLIC_URL + "/api/";
 
 export const get = async (url: string) => {
   const data = await fetcher(baseURL + url);
   return data;
 };
 
-export const post = async (
-  url: string,
-  body: { [key: string]: any } | undefined
-) => {
-  console.log(baseURL)
+export const post = async (url: string, body: { [key: string]: any} | undefined) => {
   const data = await fetcher(baseURL + url, {
     method: "POST",
     body: JSON.stringify(body),
@@ -20,10 +16,7 @@ export const post = async (
   return data;
 };
 
-export const patch = async (
-  url: string,
-  body: { [key: string]: any } | undefined
-) => {
+export const patch = async (url: string, body: { [key: string]: any} | undefined) => {
   const data = await fetcher(baseURL + url, {
     method: "PATCH",
     body: JSON.stringify(body),
