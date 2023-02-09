8adec1542a9599bfad878e8bad7861f44b205e8d
unknown <mateolewinzon@gmail.com>
2023-02-09 12:44:39 -0300

    fix rel=noreferrer

--- components/WhatsAppOverlay.tsx
+++ components/WhatsAppOverlay.tsx
@@ -9,7 +9,7 @@ export const WhatsAppOverlay = ({ phone }: Props) => {
   const i18n = useI18n();
 
   return (
-    <a target="_blank" href={`https://wa.me/${phone}`}>
+    <a rel="noreferrer" target="_blank" href={`https://wa.me/${phone}`}>
       <div className="fixed bottom-[40px] right-[40px]">
         <div className="flex bg-purple-200 rounded-full items-center outline outline-purple-800">
           <Span className="mx-3 hidden md:inline">{i18n.t("pet.contact_owner")}</Span>
